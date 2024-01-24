8c484d230cb616937a3a11cd47a70b5fcbb670ad
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2024-01-24 17:14:29 +0100

    mmmmmmmmmmm

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function myHeader() {
-  Header;
-}
+// function myHeader() {
+//   Header;
+// }
 window.onscroll = function () {
   myFunction();
 };
@@ -13,7 +13,7 @@ let sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.scrollY > sticky) {
+  if (window.scrollX > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
